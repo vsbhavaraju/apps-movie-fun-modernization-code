6900cb3c223a3f44ee16e5901fb2cc4d89d60707
vsbhavaraju <vsbhavaraju@gmail.com>
2018-03-08 15:12:32 -0700

    Implemented service discovery

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.superbiz.moviefun;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class DevelopmentSecurityConfig {
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
